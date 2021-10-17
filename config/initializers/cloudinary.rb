5cc33192caadb571e0556c7fa89140a88b80dcaa
Suraj Bhosale <sirofficial322@gmail.com>
2021-10-17 21:13:23 +0530

    Add cloudinary keys

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV["cloudinary_cloud_name"]
-  config.api_key = ENV["cloudinary_api_key"]
-  config.api_secret = ENV["cloudinary_api_secret"]
+  config.cloud_name = "sirbhosale"
+  config.api_key = "399269318957461"
+  config.api_secret = "Nayan9hsUyZRi5DFT-Dcgr73CPk"
   config.cdn_subdomain = true
 end
